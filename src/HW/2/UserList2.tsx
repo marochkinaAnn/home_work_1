a0429d29b63a78a2116814aae1347a569d9adb2b
Анна <anutochka_15@mail.ru>
2025-12-03 00:15:51 +0300

    finish_com1

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -1,5 +1,5 @@
 import {CurrentUser} from './CurrentUser';
-import {UsersObjectType, UserType} from './HW2';
+import {UsersObjectType} from './HW2';
 
 type UserList2PropsType = {
 	users:  UsersObjectType // пропиши типизацию
